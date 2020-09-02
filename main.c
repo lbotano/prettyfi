d3af106bde95aba56539dcd75d1cafc06446f854
lbotano <otanolautarob@gmail.com>
2020-09-02 12:18:08 -0300

    Added arguments

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <string.h>
 #include <inttypes.h>
 
@@ -9,13 +10,14 @@ typedef struct node {
 } node_t;
 
 const int LINE_MAX_SIZE = 10000;
-const int COLUMN_SIZE = 40;
+
+int colSize = 50;
 node_t* lines;
 
 void push(node_t* list, char* val) {
     node_t* currentNode = list;
-    while(1) {
-        if(currentNode->val == 0) {
+    while (1) {
+        if (currentNode->val == 0) {
             currentNode->val = malloc(sizeof(char) * LINE_MAX_SIZE);
             strcpy(currentNode->val, val);
             break;
@@ -28,7 +30,11 @@ void push(node_t* list, char* val) {
 }
 
 void fileToList(const char* filename, node_t* list) {
-    FILE* file = fopen(filename, "r");
+    FILE* file;
+    if (!(file = fopen(filename, "r"))) {
+        printf("Error: %s: %s\n", strerror(errno), filename);
+        exit(EXIT_FAILURE);
+    }
     char* line = NULL;
     size_t len = 0;
     while(getline(&line, &len, file) != -1) {
@@ -39,7 +45,7 @@ void fileToList(const char* filename, node_t* list) {
 
 void printList(node_t* list) {
     node_t* currentNode = list;
-    while(currentNode != NULL) {
+    while (currentNode != NULL) {
         printf("%s", currentNode->val);
         currentNode = currentNode->next;
     }
@@ -47,15 +53,15 @@ void printList(node_t* list) {
 
 void putBreakpoints(node_t* list) {
     node_t* currentNode = list;
-    while(1) {
+    while (1) {
         int i = 1;
-        int breaklinePos = COLUMN_SIZE;
+        int breaklinePos = colSize;
         while (currentNode->val[breaklinePos] != '\0') {
             if (currentNode->val[breaklinePos] == ' ') {
                 currentNode->val[breaklinePos] = '\n';
-                if (i * COLUMN_SIZE < strlen(currentNode->val)) {
+                if (i * colSize < strlen(currentNode->val)) {
                     i++;
-                    breaklinePos = i * COLUMN_SIZE;
+                    breaklinePos = i * colSize;
                 }
             } else {
                 breaklinePos++;
@@ -78,13 +84,57 @@ void listToFile(node_t* list, const char* filename) {
     fclose(file);
 }
 
+void help() {
+    printf("prettyfi 1.0 (2 Sep 2020, compiled %s %s)\n", __DATE__, __TIME__);
+    puts("lbotano 2020");
+    puts("Usage: prettyfi <file> [arguments ..]");
+    puts("Arguments:");
+    puts("-h or --help: Display this help message");
+    puts("-c <character limit>: Set the limit of characters in the file (default is 50)");
+    exit(EXIT_SUCCESS);
+}
+
+void setColSize(char* arg) {
+    if (arg == NULL) {
+        puts("Error: no character limit specified.");
+        exit(EXIT_FAILURE);
+    }
+
+    colSize = strtoumax(arg, NULL, 10);
+    if (errno == ERANGE) {
+        printf("Error: invalid character limit: %s\n", arg);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void analyzeArguments(char* argv[]) {
+    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
+        help();
+
+    int i = 2;
+    char* arg = argv[i];
+    while (arg != NULL) {
+        int argLength = 1;
+        if (strcmp(arg, "-c") == 0) {
+            argLength = 2;
+            setColSize(argv[i + 1]);
+        } else {
+            printf("Invalid argument: %s\n", arg);
+            exit(EXIT_FAILURE);
+        }
+        i += argLength;
+        arg = argv[i];
+    }
+}
+
+
 int main(int argc, char* argv[]){
     lines = (node_t*) malloc(sizeof(node_t));
 
+    analyzeArguments(argv);
     fileToList(argv[1], lines);
     putBreakpoints(lines);
-    printList(lines);
-    //listToFile(lines, argv[1]);
+    listToFile(lines, argv[1]);
 
     return 0;
 }
