b559523199b369fa7e7e99292e67244d4110b011
lbotano <otanolautarob@gmail.com>
2020-09-09 01:41:21 -0300

    Solves issue #1
    Also created a second file for less relevant functions

--- main.c
+++ main.c
@@ -1,87 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <string.h>
 #include <inttypes.h>
 
-typedef struct node {
-    char* val;
-    struct node* next;
-} node_t;
-
-const int LINE_MAX_SIZE = 10000;
+#include "utils.c"
 
 int colSize = 50;
+int doPrepend = 0;
+
 node_t* lines;
 
-void push(node_t* list, char* val) {
+
+// Puts breaklines in strings of a given list.
+void putBreaklines(node_t* list) {
     node_t* currentNode = list;
+
     while (1) {
-        if (currentNode->val == 0) {
-            currentNode->val = malloc(sizeof(char) * LINE_MAX_SIZE);
-            strcpy(currentNode->val, val);
-            break;
-        } else {
-            if (currentNode->next == NULL)
-                currentNode->next = (node_t*) malloc(sizeof(node_t));
-            currentNode = currentNode->next;
+        // Check how many tabs or spaces to put for each line
+        int prependSize = 0;
+            for (int i = 0; currentNode->val[i] == '\0' || currentNode->val[i] == '\n' || currentNode->val[i] == ' ' || currentNode->val[i] == '\t'; i++) {
+                prependSize++;
+            }
+            char prepend[prependSize + 2]; // +2 because of \n and \0
+            prepend[0] = '\n';
+        if (doPrepend) {
+            for (int i = 1; i < prependSize + 1; i++) {
+                prepend[i] = currentNode->val[i - 1];
+            }
+            prepend[prependSize + 2] = '\0';
         }
-    }
-}
-
-void fileToList(const char* filename, node_t* list) {
-    FILE* file;
-    if (!(file = fopen(filename, "r"))) {
-        printf("Error: %s: %s\n", strerror(errno), filename);
-        exit(EXIT_FAILURE);
-    }
-    char* line = NULL;
-    size_t len = 0;
-    while(getline(&line, &len, file) != -1) {
-        push(list, line);
-    }
-    fclose(file);
-}
-
-void printList(node_t* list) {
-    node_t* currentNode = list;
-    while (currentNode != NULL) {
-        printf("%s", currentNode->val);
-        currentNode = currentNode->next;
-    }
-}
-
-void putBreakpoints(node_t* list) {
-    node_t* currentNode = list;
-    while (1) {
+        
         int i = 1;
         int breaklinePos = colSize;
         while (currentNode->val[breaklinePos] != '\0') {
             if (currentNode->val[breaklinePos] == ' ') {
-                currentNode->val[breaklinePos] = '\n';
-                if (i * colSize < strlen(currentNode->val)) {
+                strcpy(currentNode->val, currentNode->val);
+                if (doPrepend) {
+                    char *result;
+                    putStringMiddle(currentNode->val, prepend, breaklinePos, &result);
+                    strcpy(currentNode->val, result);
+                } else {
+                    currentNode->val[breaklinePos] = '\n';
+                }
+                if (breaklinePos + colSize < strlen(currentNode->val)) {
                     i++;
-                    breaklinePos = i * colSize;
+                    breaklinePos += colSize;
                 }
             } else {
                 breaklinePos++;
             }
         }
 
-        if(currentNode->next == NULL) break;
+        if (currentNode->next == NULL) break;
         currentNode = currentNode->next;
     }
-}
-
-void listToFile(node_t* list, const char* filename) {
-    FILE* file = fopen(filename, "w");
-    node_t* currentNode = list;
-    while(1) {
-        fputs(currentNode->val, file);
-        if(currentNode->next == NULL) break;
-        currentNode = currentNode->next;
-    }
-    fclose(file);
 }
 
 void help() {
@@ -91,6 +63,7 @@ void help() {
     puts("Arguments:");
     puts("-h or --help: Display this help message");
     puts("-c <character limit>: Set the limit of characters in the file (default is 50)");
+    puts("-p: To take in count tabulation");
     exit(EXIT_SUCCESS);
 }
 
@@ -107,7 +80,9 @@ void setColSize(char* arg) {
     }
 }
 
+// Tokenizes the passed arguments.
 void analyzeArguments(char* argv[], int argc) {
+    // Display help if no arguments are passed
     if (argc <= 1 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
         help();
 
@@ -118,6 +93,8 @@ void analyzeArguments(char* argv[], int argc) {
         if (strcmp(arg, "-c") == 0) {
             argLength = 2;
             setColSize(argv[i + 1]);
+        } else if (strcmp(arg, "-p") == 0) {
+            doPrepend = 1;
         } else {
             printf("Invalid argument: %s\n", arg);
             exit(EXIT_FAILURE);
@@ -133,8 +110,9 @@ int main(int argc, char* argv[]){
 
     analyzeArguments(argv, argc);
     fileToList(argv[1], lines);
-    putBreakpoints(lines);
+    putBreaklines(lines);
     listToFile(lines, argv[1]);
+
 
     return 0;
 }
